a064f4648928f559f95c2292424c0ee143fd8594
Anlele1701 <lethanhduyan@gmail.com>
2024-10-08 16:18:45 +0700

    Completed Permission CRUD

--- src/main/java/com/anle/identity/service/exception/ErrorCode.java
+++ src/main/java/com/anle/identity/service/exception/ErrorCode.java
@@ -1,13 +1,15 @@
 package com.anle.identity.service.exception;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 
 @Getter
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public enum ErrorCode {
     USER_EXISTED(401, "User Existed", HttpStatus.INTERNAL_SERVER_ERROR),
     UNCATEGORIZED_EXCEPTION(408, "Uncategorized Exception",  HttpStatus.BAD_REQUEST),
@@ -16,10 +18,10 @@ public enum ErrorCode {
     PASSWORD_INVALID(401, "Password must be at least 8 characters",  HttpStatus.BAD_REQUEST),
     USER_NOT_EXISTED(401, "User is not existed",  HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(401, "User is not authenticated",  HttpStatus.UNAUTHORIZED),
-    UNAUTHORIZED(401, "Do not have permission", HttpStatus.FORBIDDEN);
-
-    private int code;
-    private String message;
-    private HttpStatusCode statusCode;
+    UNAUTHORIZED(401, "Do not have permission", HttpStatus.FORBIDDEN),
+    PERMISSION_NOT_EXISTED(401, "Permission is not existed", HttpStatus.NOT_FOUND);
+    int code;
+    String message;
+    HttpStatusCode statusCode;
 
 }
